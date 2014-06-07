fe8b681d5d855512a206eed03daca3cd578a1726
王刚 <wgybzb@sina.cn>
2014-06-07 10:58:37 +0800

    添加单元测试

---
+++ src/test/java/zx/soft/navie/bayes/mapred/TrainsVectorTest.java
@@ -0,0 +1,21 @@
+package zx.soft.navie.bayes.mapred;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TrainsVectorTest {
+
+	@Test
+	public void testTokenizeDoc() {
+		String[] words = { "cate1,cate2", "测试", "数据" };
+		assertEquals("[测试, 数据]", TrainsVector.tokenizeDoc(words).toString());
+	}
+
+	@Test
+	public void testTokenizeCates() {
+		String cates = "cate1,cate2";
+		assertEquals("[cate1, cate2]", TrainsVector.tokenizeCates(cates).toString());
+	}
+
+}
