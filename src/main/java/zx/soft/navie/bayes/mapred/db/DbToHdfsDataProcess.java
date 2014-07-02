42ed5650fda8fcbce42985477c641695678905dc
王刚 <wanggang@zxisl.com>
2014-07-02 09:35:18 +0800

    添加pom中插件assembly的版本号，去除打印日志

--- src/main/java/zx/soft/navie/bayes/mapred/db/DbToHdfsDataProcess.java
+++ src/main/java/zx/soft/navie/bayes/mapred/db/DbToHdfsDataProcess.java
@@ -66,6 +66,7 @@ public class DbToHdfsDataProcess extends Configured implements Tool {
 		FileOutputFormat.setOutputPath(job, dstDataPath);
 
 		job.setInputFormatClass(DBInputFormat.class);
+		// 是否可设置多个数据表？
 		DBInputFormat.setInput(job, DbInputWritable.class, tablename, //input table name
 				null, null, new String[] { "wid", "text" } // table columns
 				);
