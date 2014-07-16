64cebe5df77c840596c3a62262f5ea621c651296
王刚 <wanggang@zxisl.com>
2014-07-16 14:03:27 +0800

    修改验证数据处理

--- src/main/java/zx/soft/naive/bayes/mapred/txt/VerifyTxtToHdfsDataProcess.java
+++ src/main/java/zx/soft/naive/bayes/mapred/txt/VerifyTxtToHdfsDataProcess.java
@@ -5,16 +5,15 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import zx.soft.naive.bayes.mapred.input.IgnoreEofSequenceFileInputFormat;
 import zx.soft.naive.bayes.utils.HDFSUtils;
 
 public class VerifyTxtToHdfsDataProcess extends Configured implements Tool {
@@ -35,13 +34,6 @@ public class VerifyTxtToHdfsDataProcess extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 
 		Configuration conf = getConf();
-		/**
-		 * 设置Mapper输出压缩
-		 */
-		conf.setBoolean("mapred.compress.map.output", true); // 开起map输出压缩
-		conf.setClass("mapred.map.output.compression.codec", GzipCodec.class, CompressionCodec.class); // 设置压缩算法
-		int numReduceTasks = conf.getInt("numReduceTasks", 10);
-
 		Path sourceDataPath = new Path(conf.get("sourceData"));
 		Path dstDataPath = new Path(conf.get("processData"));
 
@@ -49,13 +41,12 @@ public class VerifyTxtToHdfsDataProcess extends Configured implements Tool {
 
 		Job job = new Job(conf, "Naive-Bayes-Verify-Txt-DataProcess");
 		job.setJarByClass(VerifyTxtToHdfsDataProcess.class);
-		job.setMapperClass(TxtToHdfsMapper.class);
-		job.setReducerClass(TxtToHdfsReducer.class);
+		job.setMapperClass(VerifyTxtToHdfsMapper.class);
 		// 输入的文件不是序列化文件，所以不需要设置
-		//		job.setInputFormatClass(IgnoreEofSequenceFileInputFormat.class);
-		job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(IgnoreEofSequenceFileInputFormat.class);
+		//		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(SequenceFileOutputFormat.class);
-		job.setNumReduceTasks(numReduceTasks);
+		job.setNumReduceTasks(0);
 
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(Text.class);
--- src/main/java/zx/soft/naive/bayes/mapred/txt/VerifyTxtToHdfsMapper.java
+++ src/main/java/zx/soft/naive/bayes/mapred/txt/VerifyTxtToHdfsMapper.java
@@ -8,9 +8,19 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class VerifyTxtToHdfsMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 
+	private static String[] texts;
+	private static StringBuffer sb;
+
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException {
-		context.write(key, value);
+		texts = value.toString().split("\\s");
+		sb = new StringBuffer();
+		for (String t : texts) {
+			sb.append(t).append(" ");
+		}
+		if (sb.length() > 1) {
+			context.write(key, new Text(key + " " + sb.subSequence(0, sb.length() - 1)));
+		}
 	}
 
 }
